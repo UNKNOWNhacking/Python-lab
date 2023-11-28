7bec223f864fdb7b8236f326c763d492e01cd327
UNKNOWNhacking <114736256+UNKNOWNhacking@users.noreply.github.com>
2023-11-28 15:04:50 +0530

    Add files via upload

---
+++ Python files/EX.07d.py
@@ -0,0 +1,15 @@
+# Original string
+original_string = "Hello, World!"
+
+# String to be replaced
+to_replace = "World"
+
+# Replacement string
+replacement = "Python"
+
+# Replace 'World' with 'Python' in the original string
+new_string = original_string.replace(to_replace, replacement)
+
+# Print the result
+print("Original string:", original_string)
+print("After replacement:", new_string)
